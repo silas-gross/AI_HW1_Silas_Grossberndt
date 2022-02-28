e8c9739d7dd13e39ff119daf6a83b4071cfda774
silas-gross <silas.grossberndt@gmail.com>
2022-02-27 22:15:17 -0500

    made main file

---
+++ HW1_Q1_Silas_Grossberndt/driver.py
@@ -0,0 +1,12 @@
+import numpy as np
+import resource as rs
+import sys
+import time
+
+#take in arguments of the method and board
+method=sys.argv[1] 
+board=sys.argv[2] 
+
+outfile=open("output.txt", "w")
+
+
